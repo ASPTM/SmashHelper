90a1ff81090e90eddb1d17e79771515554b0df71
David Dernovoj <david@dernovoj.com>
2017-10-11 10:15:39 +0100

    Added a basic layout for the index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("test");
+const express = require('express');
+
+const server = express();
+
+server.use('/test', async function(req, res) {
+    res.send(`<html>
+    <head></head>
+    <body><h1>test</h1></body>
+    </html>`).end();
+});
+
+server.listen(3000, '0.0.0.0');
